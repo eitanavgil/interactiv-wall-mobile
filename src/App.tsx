efaa33bc7cb7dd6e6f6dd1b6dba4a3cb0b206cb5
Eitan Avgil <eitan.avgil@kaltura.com>
2019-10-30 19:44:55 +0200

    add error screen

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import logo from './logo.png';
 import xx from './x.png';
 import send from './send.png';
 import but from './but.png';
+import sad from './sad.png';
 import {UploadManager} from "./componenets/uploader/uploadManager";
 import {KalturaClient} from "kaltura-typescript-client";
 import {KalturaMediaType} from "kaltura-typescript-client/api/types";
 import {decode} from "base64-arraybuffer";
 
-export enum PreviewMode {
+export enum AppState {
     init = "init",
     preview = "preview",
     upload = "upload",
+    error = "error",
     camera = "camera"
 }
 
@@ -26,19 +28,20 @@ const App: React.FC = () => {
 
     const webcamRef = useRef(null);
     const previewRef = useRef(null);
+    const errorScreen = useRef(null);
     const camEffect = useRef(null);
     const uploadManager = useRef(null);
-    const ks = "GFiMGRhMTI5Nzk1ZGJhMDRjMDcyODExMGYwN2ZkOGZlMjJmMjZhN3wyNzAxNzsyNzAxNzsxNTcyNTA1MzAwOzI7MTU3MjQxODkwMC44NzkyO2VpdGFuLmF2Z2lsQGthbHR1cmEuY29tO2Rpc2FibGVlbnRpdGxlbWVudCxhcHBpZDprbWM7Ow==";
+    const ks = "LIeGFiMGRhMTI5Nzk1ZGJhMDRjMDcyODExMGYwN2ZkOGZlMjJmMjZhN3wyNzAxNzsyNzAxNzsxNTcyNTA1MzAwOzI7MTU3MjQxODkwMC44NzkyO2VpdGFuLmF2Z2lsQGthbHR1cmEuY29tO2Rpc2FibGVlbnRpdGxlbWVudCxhcHBpZDprbWM7Ow==";
 
     // const [ks, setKs] = useState();
     const [client, setClient] = useState();
     const [blob, setBlob] = useState();
     const [camStatus, setCameraStatus] = useState(CameraStatus.init);
-    const [previewMode, setPreviewMode] = useState(PreviewMode.camera);
+    const [appState, setAppState] = useState(AppState.camera);
 
     const buttonClicked = (isPreview?: boolean) => {
-        if (previewMode === PreviewMode.camera && webcamRef.current) {
-            setPreviewMode(PreviewMode.preview);
+        if (appState === AppState.camera && webcamRef.current) {
+            setAppState(AppState.preview);
             // we have an active camera
             const camera: any = webcamRef.current;
             if (camera!.getScreenshot) {
@@ -47,24 +50,24 @@ const App: React.FC = () => {
                 const imageByteArray = decode(jpegBase64.split(",")[1]);
                 setBlob(imageByteArray);
             }
-        } else if (previewMode === PreviewMode.preview) {
-            setPreviewMode(PreviewMode.upload);
+        } else if (appState === AppState.preview) {
+            setAppState(AppState.upload);
         }
     };
 
     // cammera shutter effect
     useEffect(() => {
-        if (previewMode === PreviewMode.camera) {
+        if (appState === AppState.camera) {
             (previewRef.current as any).src = "";
         }
-        if (previewMode === PreviewMode.preview || previewMode === PreviewMode.camera) {
+        if (appState === AppState.preview || appState === AppState.camera) {
             // change to preview - show effect
             (camEffect.current as any).classList.add("show");
             setTimeout(() => {
                 (camEffect.current as any).classList.remove("show");
             }, 100);
         }
-    }, [previewMode]);
+    }, [appState]);
 
 
     // one time loaded
@@ -83,25 +86,38 @@ const App: React.FC = () => {
     }, []);
 
     return (
-        <div className={"App " + previewMode}>
+        <div className={"App " + appState}>
             <header className="App-header">
+                {appState === AppState.error &&
+                <div className="error" ref={errorScreen}>
+                    <div className="error-container">
+                        <div>
+                            Something went wrong
+                        </div>
+                        <div>
+                            , Please try again.
+                        </div>
+                        <img className={"sad"} src={sad} alt="Booo..."/>
+                    </div>
+                </div>
+                }
                 <div className="cam-effect" ref={camEffect}></div>
 
                 <div className="loader-container">
                     <div className="loader"></div>
                 </div>
 
-                {previewMode === PreviewMode.preview &&
+                {appState === AppState.preview &&
                 <button><img src={xx} alt="Close" className={"close-button"}
                              onClick={() => {
                                  setBlob(null);
-                                 setPreviewMode(PreviewMode.camera)
+                                 setAppState(AppState.camera)
                              }}
                 /></button>
                 }
                 <img src={logo} alt="Logo" className={"logo"}/>
                 <div className="camera-preview-container">
-                    <div className={"preview-container " + PreviewMode.preview}>
+                    <div className={"preview-container " + AppState.preview}>
                         <img id={"preview-image"} ref={previewRef}></img>
                     </div>
                     <Webcam audio={false} imageSmoothing={false}
@@ -122,26 +138,26 @@ const App: React.FC = () => {
                     <button className={"button"}
                             onClick={() => buttonClicked()}
                     >
-                        {previewMode === PreviewMode.camera &&
+                        {appState === AppState.camera &&
                         <img src={but} alt="Capture" className={"but"}/>
                         }
-                        {(previewMode === PreviewMode.preview || previewMode === PreviewMode.upload) &&
+                        {(appState === AppState.preview || appState === AppState.upload) &&
                         <img src={send} alt="Send" className={"send"}/>
                         }
                     </button>
                 }
                 {
-                    blob && client && previewMode === PreviewMode.upload &&
+                    blob && client && appState === AppState.upload &&
                     <UploadManager client={client}
                                    ref={uploadManager}
                                    mediaType={KalturaMediaType.image}
                                    recordedBlobs={[blob]}
                                    onError={(error) => {
-                                       console.log("Error:", error)
+                                       setAppState(AppState.error)
                                    }}
                                    onUploadEnded={(entryId) => {
                                        console.log(">>>> ENDED",);
-                                       setPreviewMode(PreviewMode.camera)
+                                       setAppState(AppState.camera)
                                        setBlob(null)
                                    }}
                                    onUploadStarted={(entryId) => {
